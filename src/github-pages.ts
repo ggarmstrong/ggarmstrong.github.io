ecabcc51916bfc0262784a4fbb078df30c273ffe
gene.armstrong <gene.armstrong@chroma.ca>
2022-09-10 23:25:52 -0400

    bring in svelte app refresh

--- src/github-pages.ts
+++ src/github-pages.ts
@@ -1,17 +1,17 @@
-import ghpages from 'gh-pages';
+// import ghpages from 'gh-pages';
 
-ghpages.publish(
-  'public',
-  {
-    branch: 'gh-pages',
-    repo: 'https://github.com/ggarmstrong/ggarmstrong-2022.git',
-    user: {
-      name: 'ggarmstrong',
-      email: 'gene.armstrong.hello@gmail.com'
-    },
-    dotfiles: true
-  },
-  () => {
-    console.log('Deploy to github pages complete!');
-  }
-);
+// ghpages.publish(
+//   'public',
+//   {
+//     branch: '2022-refresh',
+//     repo: 'https://github.com/ggarmstrong/ggarmstrong-2022.git',
+//     user: {
+//       name: 'ggarmstrong',
+//       email: 'gene.armstrong.hello@gmail.com'
+//     },
+//     dotfiles: true
+//   },
+//   () => {
+//     console.log('Deploy to github pages complete!');
+//   }
+// );
